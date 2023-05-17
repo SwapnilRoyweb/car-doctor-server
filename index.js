dd4b24c98a43239a657247db61cc51df81def11a
swapnilRoyWeb <swapnilroyweb@gmail.com>
2023-05-17 11:47:01 +0600

    service booking details add to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -29,9 +29,27 @@ async function run() {
 
     const serviceCollection = client.db('CarDoctors').collection('services');
 
+    const bookingCollection = client.db('CarDoctors').collection('bookings');
+
     app.get('/services', async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/services/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    })
+
+    // booking
+    app.post('/bookings', async(req, res)=> {
+      const booking = req.body;
+      // console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
       res.send(result);
     })
 
